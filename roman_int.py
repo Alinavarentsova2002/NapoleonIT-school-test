adeefba6bfb7a349f2519ab323fb0a54be43d0c1
Moses Grayson <mgrayson@at-k7.msk.21-school.ru>
2020-11-16 17:17:09 +0300

    2

--- roman_int.py
+++ roman_int.py
@@ -6,19 +6,17 @@
 #    By: mgrayson <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/16 16:55:20 by mgrayson          #+#    #+#              #
-#    Updated: 2020/11/16 17:05:20 by mgrayson         ###   ########.fr        #
+#    Updated: 2020/11/16 17:16:59 by mgrayson         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-def int_rom_2(intg):
-    keys=[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    value=['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
-    ans=""
-    i=0
-    while intg>0:
-        if keys[i]<=intg:
-            intg=intg-keys[i]
-            ans=ans+value[i]
-        else:
-            i=i+1
-    return(ans)
 
+class solution:
+    def romanToInt(self, s: str)(self, s):
+        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        int_val = 0
+        for i in range(len(s)):
+            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+            else:
+                int_val += rom_val[s[i]]
+        return int_val
